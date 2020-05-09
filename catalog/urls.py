5055d19dcfe1a6c7e61022b145d2464cd75857f1
Ershad Ramezani <ershad.ra@gmail.com>
2020-05-09 23:35:15 +0200

    second edition

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+app_name = 'catalog'
 
+urlpatterns = [
+    path('', views.index, name='index')
+    # path('home/', views.home, name='myapp-home'),
+    # path('', views.QuestionView.as_view(), name='question'),
+    # path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
 ]
